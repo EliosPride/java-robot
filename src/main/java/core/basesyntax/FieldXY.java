548baa37bbb6f8e73c7049372bd9de857fb392e5
Illia Sydorchuk <batonvagon@gmail.com>
2022-04-16 18:22:01 +0300

    Using logic java robot.

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -2,8 +2,8 @@ package core.basesyntax;
 
 /**
  * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y. 
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали 
+ * целочисленным координатами: X и Y.
+ * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали
  * графики функций в школе?)
  * В начальный момент робот находится в некоторой позиции на поле.
  * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
@@ -12,8 +12,27 @@ package core.basesyntax;
  */
 public class FieldXY {
 
-    public void moveRobot(Robot robot, int toX, int toY) {
-        //your code
+    public void moveRobot(Robot robot, int coordinateX, int coordinateY) {
+        robot.getCoordinateX();
+        robot.getCoordinateY();
+        robot.getDirection();
+        int[] toX = new int[coordinateX - robot.getCoordinateX()];
+        int[] toY = new int[coordinateY - robot.getCoordinateY()];
+        for (int i = 0; i < toX.length; i++) {
+            if (coordinateX - (robot.getCoordinateX() + 1) > coordinateX - robot.getCoordinateX()) {
+                robot.turnLeft();
+            }
+            if (coordinateX - (robot.getCoordinateX() + 1) < coordinateX - robot.getCoordinateX()) {
+                robot.stepForward();
+            }
+            for (int a = 0; a < toY.length; a++) {
+                if (coordinateY - (robot.getCoordinateY() + 1) > coordinateY - robot.getCoordinateY()) {
+                    robot.turnRight();
+                }
+                if (coordinateY - (robot.getCoordinateY() + 1) < coordinateY - robot.getCoordinateY()) {
+                    robot.stepForward();
+                }
+            }
+        }
     }
 }
-
