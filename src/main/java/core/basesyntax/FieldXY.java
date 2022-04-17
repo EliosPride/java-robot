1ec382a98238444caad962190500c15bf17584c7
Illia Sydorchuk <batonvagon@gmail.com>
2022-04-17 17:13:57 +0300

    The end of.

--- src/main/java/core/basesyntax/FieldXY.java
+++ src/main/java/core/basesyntax/FieldXY.java
@@ -1,35 +1,21 @@
 package core.basesyntax;
 
-/**
- * <p>На игровом поле находится робот. Позиция робота на поле описывается двумя
- * целочисленным координатами: X и Y.
- * Ось X смотрит слева направо, ось Y — снизу вверх.(Помните, как рисовали
- * графики функций в школе?)
- * В начальный момент робот находится в некоторой позиции на поле.
- * Также известно, куда робот смотрит: вверх, вниз, направо или налево.
- * Ваша задача — привести робота в заданную точку игрового поля.
- * Реализуйте метод moveRobot()</p>
- */
 public class FieldXY {
 
     public void moveRobot(Robot robot, int coordinateX, int coordinateY) {
-        int[] toX = new int[Math.abs(coordinateX - robot.getCoordinateX())];
-        int[] toY = new int[Math.abs(coordinateY - robot.getCoordinateY())];
-        for (int i = 0; i < toX.length; i++) {
-            if (coordinateX - (robot.getCoordinateX() + 1) >= coordinateX - robot.getCoordinateX()) {
-                robot.turnLeft();
-            }
-            if (coordinateX - (robot.getCoordinateX() + 1) < coordinateX - robot.getCoordinateX()) {
-                robot.stepForward();
-            }
-            for (int a = 0; a < toY.length; a++) {
-                if (coordinateY - (robot.getCoordinateY() + 1) >= coordinateY - robot.getCoordinateY()) {
-                    robot.turnRight();
-                }
-                if (coordinateY - (robot.getCoordinateY() + 1) < coordinateY - robot.getCoordinateY()) {
-                    robot.stepForward();
-                }
-            }
+        Direction moveDirectionX = robot.getCoordinateX() > coordinateX ? Direction.LEFT : Direction.RIGHT;
+        while (moveDirectionX != robot.getDirection()) {
+            robot.turnLeft();
+        }
+        while (robot.getCoordinateX() != coordinateX) {
+            robot.stepForward();
+        }
+        Direction moveDirectionY = robot.getCoordinateY() > coordinateY ? Direction.DOWN : Direction.UP;
+        while (moveDirectionY != robot.getDirection()) {
+            robot.turnRight();
+        }
+        while (robot.getCoordinateY() != coordinateY) {
+            robot.stepForward();
         }
     }
 }
